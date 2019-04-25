89797229b53779b0d8348d9b34303e6a4c909ebc
KendaCodes <K.Lamandier@gmail.com>
2019-04-25 10:36:51 -0400

    changes with the delete

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,10 @@ var menu= document.getElementsByClassName("fa-ellipsis-h");
 var trash = document.getElementsByClassName("fa-trash");
 
 
-;
 
 Array.from(trash).forEach(function(element) {
   element.addEventListener('click', function(){
-    console.log(trash);
-    const id = this.parentNode.parentNode.parentNode.parentNode.childNodes[1].id
+    const id = this.parentNode.parentNode.parentNode.previousElementSibling.id
     console.log('<<<<DELETING THIS ID>>>>', id);
     fetch('creations', {
       method: 'delete',
